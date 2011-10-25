df4769cad10ead41ade3356f0415beb019e576fd
Pedro Alcocer <pealco@gmail.com>
2011-10-25 04:58:46 -0400

    Computing relative frequencies.

--- subj_verb_counts.py
+++ subj_verb_counts.py
@@ -1,53 +1,51 @@
 # Pedro Alcocer
 #
-# Count subject-verb co-occurences.
+# Compute subject-verb co-occurence relative frequencies.
 
 import sys
 from glob import glob
-from collections import defaultdict
 
 sys.path += glob("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages/*.egg")
 sys.path.append("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages")
 
-import re
+from collections import defaultdict
 from nltk.parse import DependencyGraph
-
 from dumbo.lib import *
 
-class Mapper():
-    
-    def root_dependencies(self, dg): 
-        return [dg.get_by_address(node) for node in dg.root["deps"]]
-        
-    def subject(self, dg):
-        return [node for node in self.root_dependencies(dg) if node["rel"] == "SBJ"]
+def root_dependencies(dg): 
+    return [dg.get_by_address(node) for node in dg.root["deps"]]
 
-    def __call__(self, article, sentence):
+def subject_node(dg):
+    return [node for node in root_dependencies(dg) if node["rel"] == "SBJ"]
+
+def mapper(data):
+    H = defaultdict(int)
+
+    for article, dg in data:
+        subject = subject_node(dg)[0]["word"]
+        verb    = dg.root["word"]
         
-        new_sentence = []
-        sentence = sentence.split("\n")
-        for word in sentence[1:-1]:
-            columns = word.split("\t")
-            try:
-                new_columns = [columns[0], columns[2], columns[4], columns[5]]
-            except IndexError:
-                return                
-            new_sentence += ["\t".join(new_columns)]
+        H[verb][subject] += 1
         
-        s = "\n".join(new_sentence)
-                
-        dg = DependencyGraph(s)
+    for verb in H:
+        yield verb, H[verb]
         
-        if self.subject(dg):
-            subject = self.subject(dg)[0]["word"]
-            verb    = dg.root["word"]
-        
-            yield (subject, verb), 1
-            yield ("*", verb), 1
-        
-        
+
+def reducer(verb, stripes):
+    H = defaultdict(int)
+
+    for stripe in stripes:
+        for subject, count in stripe.items():
+            H[subject] += count
+    
+    total = sum(count for count in H.values())
+    
+    for subject, count in H.items():
+        yield (verb, subject), count/float(total)
+    
+    
 if __name__ == '__main__':
     import dumbo
     job = dumbo.Job()
-    job.additer(Mapper, sumreducer)
+    job.additer(mapper, reducer)
     job.run()
