244944db5c657b55d22cdd12a9a3d99e8d7950c5
Pedro Alcocer <pealco@gmail.com>
2011-10-25 04:12:28 -0400

    Storing sentences as graphs.

---
+++ make_dgs.py
@@ -0,0 +1,48 @@
+# Pedro Alcocer
+#
+# Store directed graphs.
+
+import sys
+from glob import glob
+from collections import defaultdict
+
+sys.path += glob("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages/*.egg")
+sys.path.append("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages")
+
+import re
+from nltk.parse import DependencyGraph
+
+from dumbo.lib import *
+
+class Mapper():
+    
+    def root_dependencies(self, dg): 
+        return [dg.get_by_address(node) for node in dg.root["deps"]]
+        
+    def subject(self, dg):
+        return [node for node in self.root_dependencies(dg) if node["rel"] == "SBJ"]
+
+    def __call__(self, article, sentence):
+        
+        new_sentence = []
+        sentence = sentence.split("\n")
+        for word in sentence[1:-1]:
+            columns = word.split("\t")
+            try:
+                new_columns = [columns[0], columns[2], columns[4], columns[5]]
+            except IndexError:
+                return                
+            new_sentence += ["\t".join(new_columns)]
+        
+        s = "\n".join(new_sentence)
+                
+        dg = DependencyGraph(s)
+        
+        if self.subject(dg):
+            yield article, dg
+        
+if __name__ == '__main__':
+    import dumbo
+    job = dumbo.Job()
+    job.additer(Mapper, identityreducer)
+    job.run()
