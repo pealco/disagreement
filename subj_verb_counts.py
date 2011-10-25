619bad92a98ef4d2159c095c57f3e7654d1d3c58
Pedro Alcocer <pealco@gmail.com>
2011-10-25 03:40:55 -0400

    Now counting verbs too. In-mapper reducer implemented.

--- subj_verb_counts.py
+++ subj_verb_counts.py
@@ -4,6 +4,8 @@
 
 import sys
 from glob import glob
+from collections import defaultdict
+
 sys.path += glob("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages/*.egg")
 sys.path.append("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages")
 
@@ -20,27 +22,35 @@ class Mapper():
     def subject(self, dg):
         return [node for node in self.root_dependencies(dg) if node["rel"] == "SBJ"]
 
-    def __call__(self, article, sentence):
+    def __call__(self, data):
         
-        new_sentence = []
-        sentence = sentence.split("\n")
-        for word in sentence[1:-1]:
-            columns = word.split("\t")
-            try:
-                new_columns = [columns[0], columns[2], columns[4], columns[5]]
-            except IndexError:
-                return                
-            new_sentence += ["\t".join(new_columns)]
+        H = defaultdict(int)
         
-        s = "\n".join(new_sentence)
-                
-        dg = DependencyGraph(s)
+        for article, sentence in data:
         
-        if self.subject(dg):
-            subject = self.subject(dg)[0]["word"]
-            verb    = dg.root["word"]
+            new_sentence = []
+            sentence = sentence.split("\n")
+            for word in sentence[1:-1]:
+                columns = word.split("\t")
+                try:
+                    new_columns = [columns[0], columns[2], columns[4], columns[5]]
+                except IndexError:
+                    return                
+                new_sentence += ["\t".join(new_columns)]
+            
+            s = "\n".join(new_sentence)
+                    
+            dg = DependencyGraph(s)
+            
+            if self.subject(dg):
+                subject = self.subject(dg)[0]["word"]
+                verb    = dg.root["word"]
+            
+                H[(subject, verb)] += 1
+                H[("*", verb)] += 1
         
-            yield (subject, verb), 1
+        for pair in H:
+            yield pair, H[pair]
         
 if __name__ == '__main__':
     import dumbo
