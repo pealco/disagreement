bcf17d78f7b9a8b0800b762fc6bcda01f1724527
Pedro Alcocer <pealco@gmail.com>
2011-11-07 11:18:57 -0500

    Combining grammatical and ungrammatical hashes into one script.

--- build_hashes.py
+++ build_hashes.py
@@ -63,7 +63,11 @@ class Mapper():
                     subject_deps = self.dependencies(dg, subject[0])
                     if not any([node["tag"] == "CC" for node in dg.nodelist]) and not any([node["word"] == "percent" for node in dg.nodelist]):
                         if subject[0]["tag"] in ("NN", "NNS"): 
-                            if (subject[0]["tag"] != self.expected_number[verb["word"]]):
+                            if (subject[0]["tag"] == self.expected_number[verb["word"]]):
+                                subject_verb = subject[0]["word"] + "_" + verb
+                                hash = hashlib.md5(subject_verb).hexdigest()
+                                yield hash, "gram"
+                            elif (subject[0]["tag"] != self.expected_number[verb["word"]]):
                                 subject_verb = subject[0]["word"] + "_" + verb
                                 hash = hashlib.md5(subject_verb).hexdigest()
                                 yield hash, "ungram"
--- hash_count.py
+++ hash_count.py
@@ -1,6 +1,7 @@
 # Pedro Alcocer
 #
 # Convert subject-verb pairs into hashes.
+# dumbo start hash_count.py -input /user/pealco/subj_verb_rel_freq  -output /user/pealco/subj_verb_rel_freq_hashes -overwrite yes -hadoop h -memlimit 4294967296 -numreducetasks 30
 
 import hashlib
 from dumbo.lib import *
