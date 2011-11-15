e1d2945845c1720033e892facea577760ffc2795
Pedro Alcocer <pealco@gmail.com>
2011-11-15 11:00:15 -0500

    Commenting out unused steps.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -77,9 +77,9 @@ def preposition_filter(article, sentence_dg):
 if __name__ == '__main__':
     import dumbo
     job = dumbo.Job()
-    job.additer(remove_long_sentences,  identityreducer)
-    job.additer(select_verbs,           identityreducer)
-    job.additer(find_disagreement,      identityreducer)
-#    job.additer(wordnet_filter,         identityreducer)
+#    job.additer(remove_long_sentences,  identityreducer)
+#    job.additer(select_verbs,           identityreducer)
+#    job.additer(find_disagreement,      identityreducer)
+    job.additer(wordnet_filter,         identityreducer)
 #    job.additer(preposition_filter,      identityreducer)
     job.run()
