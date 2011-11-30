2f113519380c158dc4746ab0091fd28363958a50
Pedro Alcocer <pealco@gmail.com>
2011-11-30 03:44:59 -0500

    Counting words in the corpus.

---
+++ pipeline/wordcount.py
@@ -0,0 +1,42 @@
+# Pedro Alcocer
+#
+# Wikipedia wordcount
+# Call with:
+# dumbo start wordcount.py -input /user/pealco/wikipedia_split_parsed_deduped_dgs  -output /user/pealco/disagreement_wordcount -overwrite yes -hadoop h -memlimit 4294967296 -numreducetasks 100
+
+import os, sys
+from glob import glob
+
+sys.path += glob("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages/*.egg")
+sys.path.append("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages")
+
+import nltk
+from nltk.parse import DependencyGraph
+
+from dumbo.lib import *
+from collections import defaultdict
+
+
+### Path updates.
+
+nltk.data.path += ["/fs/clip-software/nltk-2.0b9-data"]
+
+
+def mapper(data):
+    # initialize
+    H = defaultdict(int)
+    
+    # map
+    for article, dg in data:
+        for node in dg.nodelist[1:]:
+            H[node["word"]] += 1
+
+    # close
+    for word in H:
+        yield word, H[word]
+    
+if __name__ == '__main__':
+    import dumbo
+    job = dumbo.Job()
+    job.additer(mapper, sumreducer)
+    job.run()
