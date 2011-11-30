83b5c8e6ab31075d19c1f16a4a0cf4940545d822
Pedro Alcocer <pealco@gmail.com>
2011-11-30 13:45:16 -0500

    These flags are separate.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,6 +3,7 @@
 # Finding agreement errors in Wikipedia using Hadoop
 # Call with:
 # dumbo start pipeline.py -input /user/pealco/wikipedia_split_parsed_deduped_dgs  -output /user/pealco/disagreement_pipeline_copula -overwrite yes -hadoop h -memlimit 4294967296
+#-numreducetasks 100 -file braubt_tagger.pkl
 
 import os, sys
 from glob import glob
