7e6186f5f9e688b2aae3675a6501e0101da23a4d
Pedro Alcocer <pealco@gmail.com>
2011-11-30 20:39:44 -0500

    Trying to debug an error here.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -206,8 +206,9 @@ def post_verb_plural_filter(data):
     
     verb_address = sentence_dg.root["address"]
     post_verb_address = verb_address + 1
+    post_verb_word = sentence_dg.get_by_address(post_verb_address)
     
-    if sentence_dg.get_by_address(post_verb_address)['tag'] != 'NNS':
+    if post_verb_word['tag'] != 'NNS':
         return article, sentence_dg
 
 coordination_filter = preverb_filter_factory('CC', 'tag')
