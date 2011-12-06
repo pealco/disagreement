79eedd890549748482dcea9591ff2ea4c73cbc52
Pedro Alcocer <pealco@gmail.com>
2011-12-06 13:44:55 -0500

    Compute similarity measure.

---
+++ pipeline/compute_similarity.py
@@ -0,0 +1,38 @@
+# Pedro Alcocer
+#
+# Wikipedia wordcount
+# Call with:
+# dumbo start compute_similarity.py -input /user/pealco/disagreement_subj_int  -output /user/pealco/disagreement_similarity_bad -overwrite yes -hadoop h -memlimit 4294967296
+
+import os, sys
+from glob import glob
+
+sys.path += glob("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages/*.egg")
+sys.path.append("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages")
+
+import nltk
+from nltk.parse import DependencyGraph
+
+from dumbo.lib import *
+from collections import defaultdict
+
+
+### Path updates.
+
+nltk.data.path += ["/fs/clip-software/nltk-2.0b9-data"]
+from nltk.corpus import wordnet as wn
+
+def mapper(data):
+    pair, sentence = data
+    subject, intervener = pair
+    
+    subject_synset, intervener_synset = wn.synsets(subject)[0], wn.synset(intervener)[0]
+    
+    yield pair, subject_synset.wup_similarity(intervener_synset)
+    
+
+if __name__ == '__main__':
+    import dumbo
+    job = dumbo.Job()
+    job.additer(mapper, identityreducer)
+    job.run()
