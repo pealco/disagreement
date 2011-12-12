717d7b357ff6d0e79b4a57565acd11b1e978d961
Pedro Alcocer <pealco@gmail.com>
2011-12-11 22:16:15 -0500

    Indenting fix.

--- pipeline/convert_to_sentence.py
+++ pipeline/convert_to_sentence.py
@@ -40,50 +40,50 @@ class Sentence(object):
         self.intervenor = self._find_intervenor()
         self.grammatical = self._find_grammaticality()
         
-        def _plaintext(self):
-            s = " ".join([node["word"] for node in self.dg.nodelist[1:]])
-            self.sentence = re.sub(self.punct_re, r'\g<1>', s)
+    def _plaintext(self):
+        s = " ".join([node["word"] for node in self.dg.nodelist[1:]])
+        self.sentence = re.sub(self.punct_re, r'\g<1>', s)
+    
+    def wup_similarity(self):
+        """Compute Wu-Palmer similarity."""
+        try:
+            subject_synset = wn.synsets(self.subject)[0]
+            intervenor_synset = wn.synsets(self.intervenor)[0]
+            similarity = subject_synset.wup_similarity(intervenor_synset)
+            return sentence, (grammaticality, similarity, subject, intervenor) 
+        except:
+            return False
+    
+    def _find_intervenor(self):
         
-        def wup_similarity(self):
-            """Compute Wu-Palmer similarity."""
-            try:
-                subject_synset = wn.synsets(self.subject)[0]
-                intervenor_synset = wn.synsets(self.intervenor)[0]
-                similarity = subject_synset.wup_similarity(intervenor_synset)
-                return sentence, (grammaticality, similarity, subject, intervenor) 
-            except:
-                return False
-        
-        def _find_intervenor(self):
-            
-            subject_deps = self.subject[0]['deps']
-            prepositions = [self.dg.get_by_address(dep) for dep in subject_deps if self.dg.get_by_address(dep)['tag'] == 'IN']
-            first_prep = prepositions[0]
-            intervenor = self.dg.get_by_address(first_prep['deps'][0])
-            return intervenor
-        
-        def _root_dependencies(dg): 
-            return [dg.get_by_address(node) for node in dg.root["deps"]]
-
-        def _dependencies(dg, node): 
-            return [dg.get_by_address(dep) for dep in dg.get_by_address(node["address"])["deps"]]
-
-        def _find_subject(self):
-            return [node for node in root_dependencies(self.dg) if node["rel"] == "SBJ"]
-        
-        def _find_grammaticality(self):
-            subject_tag = self.subject[0]["tag"]
-            verb = self.dg.root
-            verb_tag = self.dg.root["tag"]
-
-            if subject_tag in NUMBER and verb_tag in NUMBER:
-                if NUMBER[subject_tag] == NUMBER[verb_tag]:
-                    self.grammatical = True
-                elif NUMBER[subject_tag] != NUMBER[verb_tag]:
-                    self.grammatical = False
-        
-        def __str__(self):
-            return self.sentence
+        subject_deps = self.subject[0]['deps']
+        prepositions = [self.dg.get_by_address(dep) for dep in subject_deps if self.dg.get_by_address(dep)['tag'] == 'IN']
+        first_prep = prepositions[0]
+        intervenor = self.dg.get_by_address(first_prep['deps'][0])
+        return intervenor
+    
+    def _root_dependencies(dg): 
+        return [dg.get_by_address(node) for node in dg.root["deps"]]
+    
+    def _dependencies(dg, node): 
+        return [dg.get_by_address(dep) for dep in dg.get_by_address(node["address"])["deps"]]
+    
+    def _find_subject(self):
+        return [node for node in root_dependencies(self.dg) if node["rel"] == "SBJ"]
+    
+    def _find_grammaticality(self):
+        subject_tag = self.subject[0]["tag"]
+        verb = self.dg.root
+        verb_tag = self.dg.root["tag"]
+    
+        if subject_tag in NUMBER and verb_tag in NUMBER:
+            if NUMBER[subject_tag] == NUMBER[verb_tag]:
+                self.grammatical = True
+            elif NUMBER[subject_tag] != NUMBER[verb_tag]:
+                self.grammatical = False
+    
+    def __str__(self):
+        return self.sentence
             
 def mapper(article, dg):
     yield Sentence(article, dg), ''
