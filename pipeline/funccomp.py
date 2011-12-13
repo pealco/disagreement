5a3295f62ca476589513f1dd2018d3407e7122c4
Pedro Alcocer <pealco@gmail.com>
2011-12-13 00:47:13 -0500

    Moving function composition code to its own module.

---
+++ pipeline/funccomp.py
@@ -0,0 +1,20 @@
+from functools import partial
+
+class _compfunc(partial):
+    def __add__(self, y):
+        f = lambda *args, **kwargs: y(self.func(*args, **kwargs)) 
+        return _compfunc(f)
+
+def composable(f):
+    return _compfunc(fail_gracefully(f))
+    
+def compose(functions):
+    return reduce(lambda x, y: x + y, functions)
+
+def fail_gracefully(func):
+    def wrapper(data):
+        if data:
+            return func(data)
+        else:
+            return False
+    return wrapper
