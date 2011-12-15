1883b6ce1271c05565493dcff27a605d7bb383c6
Pedro Alcocer <pealco@gmail.com>
2011-12-15 00:54:45 -0500

    Composition functions now accept more than one argument.

--- pipeline/funccomp.py
+++ pipeline/funccomp.py
@@ -2,7 +2,7 @@ from functools import partial
 
 class _compfunc(partial):
     def __add__(self, y):
-        f = lambda *args, **kwargs: y(self.func(*args, **kwargs)) 
+        f = lambda *args, **kwargs: y(*self.func(*args, **kwargs)) 
         return _compfunc(f)
 
 def composable(f):
@@ -12,9 +12,9 @@ def compose(functions):
     return reduce(lambda x, y: x + y, functions)
 
 def fail_gracefully(func):
-    def wrapper(data):
-        if data:
-            return func(data)
+    def wrapper(*args):
+        if args[0] is False:
+            return False
         else:
             return False
     return wrapper
