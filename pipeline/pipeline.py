6c260cef4d9a10af3a36c147d891028c3f1d96d8
Pedro Alcocer <pealco@gmail.com>
2011-12-15 13:45:13 -0500

    Enabling last filters.

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -239,8 +239,8 @@ def pipeline(article, sentence_dg):
                       post_verb_plural_filter,
                       wordnet_filter,
                       preposition_filter,
-#                      keep_singular_subjects,
-#                      keep_plural_intervenors,
+                      keep_singular_subjects,
+                      keep_plural_intervenors,
                       ]
     
     composed_pipeline = compose(pipeline_steps)
