40770fc904e944e4c0b63fe31c94bbd0d01f26fc
Pedro Alcocer <pealco@gmail.com>
2011-12-15 21:49:18 -0500

    Adding a `verb` attribute to the `Sentence` class.

--- pipeline/sentence.py
+++ pipeline/sentence.py
@@ -1,23 +1,26 @@
 import re
 from nltk.corpus import wordnet as wn
 from constants import NUMBER
+
+
 class Sentence(object):
 
     def __init__(self, article, dg):
-        
+
         self.punct_re = re.compile(r'\s([,\.;\?])')
 
-        self.dg = dg    
+        self.dg = dg
         self.sentence = self._plaintext()
         self.subject = self._find_subject()
         self.intervenor = self._find_intervenor()
+        self.verb = self.dg.root
         self.grammatical = self._find_grammaticality()
         self.wup_similarity = self._wup_similarity()
-        
+
     def _plaintext(self):
         s = " ".join([node["word"] for node in self.dg.nodelist[1:]])
         return re.sub(self.punct_re, r'\g<1>', s)
-    
+
     def _wup_similarity(self):
         """Compute Wu-Palmer similarity."""
         try:
@@ -27,9 +30,9 @@ class Sentence(object):
             return similarity
         except:
             return None
-    
+
     def _find_intervenor(self):
-        
+
         subject_deps = self.subject[0]['deps']
         prepositions = [self.dg.get_by_address(dep) for dep in subject_deps if self.dg.get_by_address(dep)['tag'] == 'IN']
         try:
@@ -37,28 +40,27 @@ class Sentence(object):
             intervenor = self.dg.get_by_address(first_prep['deps'][0])
         except IndexError:
             return None
-        
+
         return intervenor
-    
-    def _root_dependencies(self, dg): 
+
+    def _root_dependencies(self, dg):
         return [dg.get_by_address(node) for node in dg.root["deps"]]
-    
-    def _dependencies(dg, node): 
+
+    def _dependencies(dg, node):
         return [dg.get_by_address(dep) for dep in dg.get_by_address(node["address"])["deps"]]
-    
+
     def _find_subject(self):
-        return [node for node in self._root_dependencies(self.dg) if node["rel"] == "SBJ"]
-    
+        return [node for node in self._root_dependencies(self.dg) if node["rel"] == "SBJ"][0]
+
     def _find_grammaticality(self):
         subject_tag = self.subject[0]["tag"]
-        verb = self.dg.root
-        verb_tag = self.dg.root["tag"]
-    
+        verb_tag = self.verb["tag"]
+
         if subject_tag in NUMBER and verb_tag in NUMBER:
             if NUMBER[subject_tag] == NUMBER[verb_tag]:
                 return True
             elif NUMBER[subject_tag] != NUMBER[verb_tag]:
                 return False
-    
+
     def __str__(self):
         return self.sentence
