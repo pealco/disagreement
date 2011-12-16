c656dafe5f363a4511fa74cdc723a8f7f18ec1c3
Pedro Alcocer <pealco@gmail.com>
2011-12-15 21:40:23 -0500

    Spacing fixes.

--- pipeline/funccomp.py
+++ pipeline/funccomp.py
@@ -1,15 +1,19 @@
 from functools import partial
 
+
 class _compfunc(partial):
     def __add__(self, y):
-        f = lambda *args, **kwargs: y(self.func(*args, **kwargs)) 
+        f = lambda *args, **kwargs: y(self.func(*args, **kwargs))
         return _compfunc(f)
+
 
 def composable(f):
     return _compfunc(fail_gracefully(f))
-    
+
+
 def compose(functions):
     return reduce(lambda x, y: x + y, functions)
+
 
 def fail_gracefully(func):
     def wrapper(data):
