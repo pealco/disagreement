02f8099a89b9ff8929c0c9ceb84756d4fbee2c45
Pedro Alcocer <pealco@gmail.com>
2012-03-03 18:57:14 -0500

    Doing word count on lowercase words.

--- pipeline/wordcount.py
+++ pipeline/wordcount.py
@@ -1,41 +1,49 @@
+"""
+
 # Pedro Alcocer
 #
 # Wikipedia wordcount
 # Call with:
-# dumbo start wordcount.py -input /user/pealco/wikipedia_split_parsed_deduped_dgs  -output /user/pealco/disagreement_wordcount -overwrite yes -hadoop h -memlimit 4294967296
 
-import os, sys
+dumbo start wordcount.py \
+    -input /user/pealco/wikipedia_split_parsed_deduped_dgs\
+    -output /user/pealco/disagreement_wordcount \
+    -overwrite yes -hadoop h -memlimit 4294967296
+
+"""
+
+import sys
 from glob import glob
 
 sys.path += glob("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages/*.egg")
 sys.path.append("/fs/clip-software/python-contrib-2.7.1.0/lib/python2.7/site-packages")
 
 import nltk
-from nltk.parse import DependencyGraph
 
 from dumbo.lib import *
 from collections import defaultdict
-
 
 ### Path updates.
 
 nltk.data.path += ["/fs/clip-software/nltk-2.0b9-data"]
 from nltk.corpus import wordnet as wn
 
+
 def mapper(data):
     # initialize
-    H = defaultdict(int)
-    
+    counts = defaultdict(int)
+
     # map
     for article, dg in data:
         for node in dg.nodelist[1:]:
-            if wn.synsets(node["word"]):
-                H[node["word"]] += 1
+            word = node["word"].lower()
+            if wn.synsets(word):
+                counts[word] += 1
 
     # close
-    for word in H:
-        yield word, H[word]
-    
+    for word in counts:
+        yield word, counts[word]
+
 if __name__ == '__main__':
     import dumbo
     job = dumbo.Job()
